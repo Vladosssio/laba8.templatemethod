fad52edd66fbccde94c7c9b60199b68371c83497
Vladosssio <valdila228@gmail.com>
2026-06-10 13:25:12 +0300

    падаюищц тест

--- test_main.py
+++ test_main.py
@@ -5,7 +5,7 @@ def test_tea_preparation():
     tea = Tea()
     assert tea.is_ready == False
     tea.prepare_recipe()
-    assert tea.is_ready == Fasle
+    assert tea.is_ready == False
 
 def test_coffee_preparation():
     coffee = Coffee()
