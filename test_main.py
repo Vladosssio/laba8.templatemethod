9b1ddc65ad95a3a013b68cc638c7e81f2932d683
Vladosssio <valdila228@gmail.com>
2026-06-10 13:27:02 +0300

    Снова работающий тест

--- test_main.py
+++ test_main.py
@@ -5,7 +5,7 @@ def test_tea_preparation():
     tea = Tea()
     assert tea.is_ready == False
     tea.prepare_recipe()
-    assert tea.is_ready == False
+    assert tea.is_ready == True
 
 def test_coffee_preparation():
     coffee = Coffee()
