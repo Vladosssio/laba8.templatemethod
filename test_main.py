10d5f47465ccb822b721fc6cca4af2f2d692ae4f
Vladosssio <valdila228@gmail.com>
2026-06-10 13:10:48 +0300

    тесты

--- test_main.py
+++ test_main.py
@@ -1,1 +1,14 @@
 
+from main import Tea, Coffee
+
+def test_tea_preparation():
+    tea = Tea()
+    assert tea.is_ready == False
+    tea.prepare_recipe()
+    assert tea.is_ready == True
+
+def test_coffee_preparation():
+    coffee = Coffee()
+    assert coffee.is_ready == False
+    coffee.prepare_recipe()
+    assert coffee.is_ready == True
